d4b5e88afc540297075c77c9904cf9b3af0c2c36
Amanda Murphy <helloamandamurphy@gmail.com>
2026-08-25 23:56:55 -0400

    Hello World file

---
+++ README.md
@@ -0,0 +1,3 @@
+# CSCI-C-343 Assignments and Sandbox
+
+Building out a class repo for assignments and to mess around with C++ since it's been a minute.
---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+// holy cow I'm writing C++ again
+# include <iostream>
+using namespace std; 
+
+int main()
+{
+    cout << "Ground Control to Major Tom." << endl;
+    cout << "Hello World." << endl;
+    cout << "Omg thank god for using semicolons again." << endl;
+}
