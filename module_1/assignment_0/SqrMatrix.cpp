56b9bd804249ec54bfd5200fd3509d2aeb685de2
Amanda Murphy <helloamandamurphy@gmail.com>
2026-08-30 17:05:00 -0400

    Update constructors to use putElement() instead of hardcoding it

--- module_1/assignment_0/SqrMatrix.cpp
+++ module_1/assignment_0/SqrMatrix.cpp
@@ -10,25 +10,26 @@ SqrMatrix::SqrMatrix() {
     sq = new int*[size]; // create a pointer to an array with a length of size
     for (int i = 0; i < size; i++) { // while i is < size
         sq[i] = new int[size]; // set the value of the pointer at index i to an array with the length of size.
-        for (int j = 0; j < N; j++) {
-            sq[i][j] = 0; // create a matrix of 0's before the user enters values
+        for (int j = 0; j < size; j++) {
+            putElement(0, i, j); // create a matrix of 0's before the user enters values
         }
     }
 }
 
 // Constructor with integer input of N, set size to N
 SqrMatrix::SqrMatrix(const int N) {
-    size = N;
     // Square Matrix of arrays (example from video lecture)
+    size = N;
     sq = new int* [N];
     for (int i = 0; i < N; i++) {
         sq[i] = new int[N];
         for (int j = 0; j < N; j++) {
-            sq[i][j] = 0; // create a matrix of 0's before the user enters values
+            putElement(0, i, j);
         }
     }
 }
 
+// Overloaded << operator for output
 std::ostream& operator << (std::ostream& out, const SqrMatrix* matrix){
     for (int i = 0; i < matrix->size; i++) {
         for (int j = 0; j < matrix->size; j++) {
@@ -39,6 +40,7 @@ std::ostream& operator << (std::ostream& out, const SqrMatrix* matrix){
     return out;
 }
 
+// Overloaded >> operator for input
 std::istream& operator >> (std::istream& in, SqrMatrix* matrix){
     for (int i = 0; i < matrix->size; i++) {
         std::cout << "Enter " << matrix->size << " values for the row " << i << std::endl;
@@ -58,17 +60,19 @@ SqrMatrix* SqrMatrix::operator * ( SqrMatrix* smPtrB) {
     for(int i = 0; i < size; ++i)
         for(int j = 0; j < size; ++j)
             for(int k = 0; k < size; ++k) {
-                m3->sq[i][j] += sq[i][k] * smPtrB-> sq[k][j];
+                m3->sq[i][j] += sq[i][k] * smPtrB-> sq[k][j]; //m
             }
     return m3;
 }
 
+// return the value at row i and column j
 int SqrMatrix::getElement(int i, int j){
-    return sq[i][j]; // return the value at row i and column j
+    return sq[i][j];
 }
 
+// set value of row i and column j
 void SqrMatrix::putElement(int val, int i, int j){
-    sq[i][j] = val; // set value of row i and column j
+    sq[i][j] = val;
 }
 
 
