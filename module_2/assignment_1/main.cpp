442ddebfa68bdc9f335fa50a163d8cd1894ccf7e
Amanda Murphy <helloamandamurphy@gmail.com>
2026-09-12 12:23:28 -0400

    Replace generated main.cpp file with the one provided for the assignment

--- module_2/assignment_1/main.cpp
+++ module_2/assignment_1/main.cpp
@@ -1,18 +1,58 @@
 #include <iostream>
-
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
+#include "src/Sequence.cpp"
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+    auto seq = std::make_unique<Sequence<int>>();
+    int v1 = 37;
+    int v2 = 29;
+    int v3 = 52;
+    //test add
+    seq->add(v1, 0);
+    seq->add(v2, 0);
+    seq->add(v3, 0);
 
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    //Todo: output sequence here.
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+    auto seq1 = std::make_unique<Sequence<std::string>>();
+    std::string value = "fish";
+    std::string value2 = "sun";
+    std::string value3 = "cat";
+    std::string value4 = "dog";
+    //Todo: output sequence here.
+    //Todo: output the entry at position 1 here.
+    //Todo: output the length of seq1 here
+
+    seq1->add(value, 0);
+    seq1->add(value2, 1);
+    seq1->add(value3, 2);
+    seq1->add(value4, 3);
+    //test remove using seq1
+    int removed = 5;
+    seq->remove(removed, 0);
+
+    auto seq2 = std::make_unique<Sequence<int>>();
+    auto seq3 = std::make_unique<Sequence<int>>();
+     v1 = 5;
+     v2 = 4;
+     v3 = 6;
+    //test transferFrom with seq2 and seq3
+    seq2->add(v1, 0);
+    seq2->add(v2, 1);
+    seq2->add(v3, 2);
+    seq3->transferFrom(*seq2);
+    //Todo: output seq3 here. must be the same as seq2
+
+    Sequence<int> seq4;
+    Sequence<int> seq5 ;
+    v1 = 5;
+    v2 = 4;
+    v3 = 6;
+    //test transferFrom with seq2 and seq3
+    seq4.add(v1, 0);
+    seq4.add(v2, 1);
+    seq5.add(v3, 2);
+    seq5 = seq4; //this gives an error if the operator= is not implemented.
+    //Todo: output seq3 here. must be the same as seq2
+
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
